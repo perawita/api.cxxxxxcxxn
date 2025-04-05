41e04f537be44b05165419a550495a21f559cf36
Ida Bagus Perawita Yasa <perawitayasa@gmail.com>
2025-04-06 04:15:59 +0800

    Test debug

--- app/services/AkrabService.js
+++ app/services/AkrabService.js
@@ -4,35 +4,36 @@ const qs = require('qs');
 
 const AkrabService = {
     purchase: async (keyAccess, userId, product_id, customer_no, customer_name, admin_name) => {
-        try {
-            const url = `${config.backend}/akrab-otomatis.php`;
+        return 'HAHAHAHAHAA';
+        // try {
+        //     const url = `${config.backend}/akrab-otomatis.php`;
 
-            const response = await axios.post(url, qs.stringify({
-                buy_kuota: '',
-                user_id: userId,
-                customer_no: customer_no,
-                product_id: product_id,
-                key_access: keyAccess,
-                customer_name : customer_name,
-                admin_name: admin_name
-            }), {
-                headers: {
-                  "cache-control": "no-store, no-cache, must-revalidate",
-                  "content-encoding": "zstd",
-                  "content-security-policy": "upgrade-insecure-requests",
-                  "content-type": "application/x-www-form-urlencoded",
-                  "Connection": "keep-alive",
-                  "pragma": "no-cache",
-                  "vary": "Accept-Encoding"
-                }
-            });
+        //     const response = await axios.post(url, qs.stringify({
+        //         buy_kuota: '',
+        //         user_id: userId,
+        //         customer_no: customer_no,
+        //         product_id: product_id,
+        //         key_access: keyAccess,
+        //         customer_name : customer_name,
+        //         admin_name: admin_name
+        //     }), {
+        //         headers: {
+        //           "cache-control": "no-store, no-cache, must-revalidate",
+        //           "content-encoding": "zstd",
+        //           "content-security-policy": "upgrade-insecure-requests",
+        //           "content-type": "application/x-www-form-urlencoded",
+        //           "Connection": "keep-alive",
+        //           "pragma": "no-cache",
+        //           "vary": "Accept-Encoding"
+        //         }
+        //     });
 
-            return response.data;
-        } catch (error) {
-            return {
-                status: false,
-                message: error.response ? error.response.data : error.message
-            };
+        //     return response.data;
+        // } catch (error) {
+        //     return {
+        //         status: false,
+        //         message: error.response ? error.response.data : error.message
+        //     };
         }
     },
     

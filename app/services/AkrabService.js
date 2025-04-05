eceda9702da281cc3e22277bbeda799c459c6c23
Ida Bagus Perawita Yasa <perawitayasa@gmail.com>
2025-04-06 04:31:01 +0800

    TEST DEBUG V8

--- app/services/AkrabService.js
+++ app/services/AkrabService.js
@@ -7,29 +7,30 @@ const AkrabService = {
         
         try {
             const url = `${config.backend}/akrab-otomatis.php`;
-            return `{keyAccess, userId, product_id, customer_no, customer_name, admin_name} HAHAHAHA V7`;
 
-        //     const response = await axios.post(url, qs.stringify({
-        //         buy_kuota: '',
-        //         user_id: userId,
-        //         customer_no: customer_no,
-        //         product_id: product_id,
-        //         key_access: keyAccess,
-        //         customer_name : customer_name,
-        //         admin_name: admin_name
-        //     }), {
-        //         headers: {
-        //           "cache-control": "no-store, no-cache, must-revalidate",
-        //           "content-encoding": "zstd",
-        //           "content-security-policy": "upgrade-insecure-requests",
-        //           "content-type": "application/x-www-form-urlencoded",
-        //           "Connection": "keep-alive",
-        //           "pragma": "no-cache",
-        //           "vary": "Accept-Encoding"
-        //         }
-        //     });
+            const response = await axios.post(url, qs.stringify({
+                buy_kuota: '',
+                user_id: userId,
+                customer_no: customer_no,
+                product_id: product_id,
+                key_access: keyAccess,
+                customer_name : customer_name,
+                admin_name: admin_name
+            }), {
+                headers: {
+                    "cache-control": "no-store, no-cache, must-revalidate",
+                    "content-encoding": "zstd", // hati-hati, ini bisa bermasalah!
+                    "content-security-policy": "upgrade-insecure-requests",
+                    "content-type": "application/x-www-form-urlencoded",
+                    "Connection": "keep-alive",
+                    "pragma": "no-cache",
+                    "vary": "Accept-Encoding"
+                },
+                timeout: 10000 // <--- Tambahkan ini
+            });
+            
 
-        //     return response.data;
+            return response.data;
         } catch (error) {
             return {
                 status: false,

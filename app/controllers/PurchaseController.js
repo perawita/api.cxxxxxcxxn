dad76f6f630e02827a0855f5225e8da597bba44c
Ida Bagus Perawita Yasa <perawitayasa@gmail.com>
2025-04-24 07:43:59 +0800

    V3

--- app/controllers/PurchaseController.js
+++ app/controllers/PurchaseController.js
@@ -27,7 +27,7 @@ const PurchaseController = {
 
 
             if (!productData) {
-                return res.status(404).json({ status: false, message: `Product is not found ${productData}` });
+                return res.status(404).json({ status: false, message: `Product is not found ${productData ?? productId}` });
             }
 
             if (productData.sisa_slot <= 0) {
